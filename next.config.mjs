7b234eea5a7914cb415422e6a3e9095077cd8146
bilgekaana <berberoglubilgekaan@gmail.com>
2024-07-08 19:57:00 +0300

    Added Image photos

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,14 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
                 pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '**'
             }
         ]
+
     }
 };
 
